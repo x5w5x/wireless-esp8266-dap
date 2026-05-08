0d8533b68cbb9f84ddd5f698c139a96974be5c63
轩 <498148475@qq.com>
2026-05-08 22:07:48 +0800

    Remove unused WiFi SSIDs and change static IP usage

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -15,8 +15,6 @@ static struct {
     const char *ssid;
     const char *password;
 } wifi_list[] __attribute__((unused)) = {
-    {.ssid = "OTA", .password = "12345678"},
-    {.ssid = "DAP", .password = "12345678"},
      {.ssid = "Xiaomi_F4D1", .password = "LinuxisUbuntu18"},
     // Add your WAP like this:
     // {.ssid = "your ssid", .password = "your password"},
@@ -30,7 +28,7 @@ static struct {
 #define MDNS_INSTANCE "DAP mDNS"
 //
 
-#define USE_STATIC_IP 1
+#define USE_STATIC_IP 0
 // If you don't want to specify the ip configuration, then ignore the following items.
 #define DAP_IP_ADDRESS 192, 168, 137, 123
 #define DAP_IP_GATEWAY 192, 168, 137, 1
